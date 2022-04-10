6abf001043eded83a83c80b074b6e26f5d289fda
devferno <devfernouny@gmail.com>
2022-04-10 06:49:59 +0000

    design and layout

---
+++ src/Pages/index.js
@@ -0,0 +1,7 @@
+import Signin from "./Signin";
+import Product from "./Product";
+import Users from "./Users";
+import Home from "./Home";
+import AddProduct from "./AddProduct";
+
+export {Signin,Product,Users,Home,AddProduct};
