bd4738988f532e1485322008a5d3c58acc7d71c1
devferno <devfernouny@gmail.com>
2022-04-10 08:15:21 +0000

    layout and routes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,8 +15,10 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import { HomeOutlined } from '@mui/icons-material';
+import {Link} from "react-router-dom";
 
 const drawerWidth = 240;
 
@@ -116,24 +118,15 @@ export default function PersistentDrawerLeft({children}) {
         </DrawerHeader>
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          {['Home','Users','Products'].map((text, index) => (
+            <Link to={`/${text!=="Home"?text:""}`} style={{textDecoration:"none"}}>
             <ListItem button key={text}>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {text==='Home'?<HomeOutlined/>:text==='Users'?<PersonOutlinedIcon/>:<ShoppingBagOutlinedIcon/>}
               </ListItemIcon>
-              <ListItemText primary={text} />
+              <ListItemText sx={{color:"black"}} primary={text} />
             </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
+            </Link>
           ))}
         </List>
       </Drawer>
