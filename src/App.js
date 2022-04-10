0c8822318dca2b36efaadadf9ee80da37c412b51
devferno <devfernouny@gmail.com>
2022-04-10 08:02:19 +0000

    signin

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react'
-import {Routes,Route} from "react-router-dom";
+import {Routes,Route, Outlet, Navigate} from "react-router-dom";
 import {Home,Users,Signin,Product,AddProduct} from "./Pages";
+import Layout from "./components/Layout";
+import axios from "axios";
+
+axios.defaults.baseURL ="http://localhost:5000";
+
+const PrivateRoute=()=>{
+    let isAuth = localStorage.getItem("access-admin");
+    return isAuth?<Layout><Outlet/></Layout> : <Navigate to="/signin"/>
+}
 
 const App = () => {
+
+  
+
   return (
+    
     <Routes>
+        <Route element={<PrivateRoute/>}>
         <Route path="/" element={<Home/>}/>
         <Route path="/products" element={<Product/>}/>
         <Route path="/users" element={<Users/>}/>
         <Route path="/products/add" element={<AddProduct/>}/>
+        </Route>
         <Route path="/signin" element={<Signin/>}/>
     </Routes>
+   
   )
 }
 
--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,9 +1,61 @@
-import React from 'react'
-
+import {useState} from "react";
+import { Container, Box, Button, Typography, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 const Signin = () => {
-  return (
-    <div>Signin</div>
-  )
-}
+  const formStyle = {
+    display: "flex",
+    p: 4,
+    mx: "auto",
+    maxWidth: "500px",
+    border: "1px solid #ddd",
+    my: 6,
+    flexDirection: "column",
+    justifyContent: "space-around",
+  };
+  const inputStyle = { my: 1 };
+  const buttonStyle = { my: 2 };
 
-export default Signin
+  const [admin,setAdmin]=useState({});
+  const handleChange=e=>{
+      const {name,value} = e.target;
+      setAdmin(prev=>({...prev,[name]:value}));
+  }
+  const navigate = useNavigate();
+  const handleSubmit=e=>{
+      e.preventDefault();
+      axios.post("/admin/signin",admin).then(res=>{localStorage.setItem("access-admin",res.data.access);navigate("/")}).catch(err=>console.log(err));
+  }
+  return (
+    <Container>
+      <Box component="form" onSubmit={handleSubmit} sx={formStyle}>
+        <Typography variant="h4" sx={{ my: 2 }}>
+          Sign in
+        </Typography>
+        <TextField
+          variant="outlined"
+          sx={inputStyle}
+          label="login"
+          type="text"
+          name="login"
+          value={admin.login}
+          onChange={handleChange }
+        />
+        <TextField
+          variant="outlined"
+          name="password"
+          value={admin.password}
+          onChange={handleChange}
+          sx={inputStyle}
+          label="password"
+          type="password"
+        />
+        <Button variant="contained" type="submit" disableElevation sx={buttonStyle}>
+          Sign in
+        </Button>
+      </Box>
+    </Container>
+  );
+};
+
+export default Signin;
