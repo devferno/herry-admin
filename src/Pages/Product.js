2063a89ecb6f3606461f967ffe125a43a13d272b
devferno <devfernouny@gmail.com>
2022-04-12 14:38:46 +0000

    add image product and filter and delete

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,11 +1,27 @@
-import React from "react";
-import SearchBar from "../components/SearchBar";
-import { Box, Button } from "@mui/material";
-import { ShoppingBagOutlined } from "@mui/icons-material";
+import {useEffect,useState} from "react";
+
+import { Box, Button,TextField } from "@mui/material";
+import { ShoppingBagOutlined ,SearchOutlined} from "@mui/icons-material";
 import ProductTable from "../components/ProductTable";
 import { Link } from "react-router-dom";
+import axios from "axios"
 
 const Product = () => {
+  const [products,setProducts] = useState([]);
+  const [filtered,setFiltered] = useState([]);
+  const [name,setName] = useState("");
+
+  useEffect(()=>{
+    axios.get("/product/").then(res=>{setProducts(res.data);setFiltered(res.data)}).catch(err=>console.log(err));
+  },[]);
+
+
+
+  const filterProduct=(e)=>{
+    e.preventDefault();
+    let newProducts = products.filter(item=>item.name===name);
+    setFiltered(newProducts);
+  }
   return (
     <Box>
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
@@ -20,9 +36,12 @@ const Product = () => {
             Add Product
           </Button>
         </Link>
-        <SearchBar />
+        <Box component="form" onSubmit={filterProduct} sx={{display:"flex",alignItems:"center"}}>
+        <TextField variant="outlined" size="small" label="name" value={name} onChange={(e)=>setName(e.target.value)}/>
+        <Button type="submit" ><SearchOutlined/></Button>
+    </Box>
       </Box>
-      <ProductTable />
+      <ProductTable filtered={filtered}/>
     </Box>
   );
 };
--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,33 +1,49 @@
-import { Button, TableBody,Table,TableHead,TableRow,Paper,TableContainer, TableCell } from '@mui/material'
-import React from 'react'
+import { Button, TableBody,Table,TableHead,TableRow,Paper,TableContainer, TableCell } from '@mui/material';
+
+import axios from "axios";
+
 import ModeOutlinedIcon from '@mui/icons-material/ModeOutlined';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import {Buffer }from "buffer";
 
-const ProductTable = () => {
+const ProductTable = ({filtered}) => {
+  
+  const deleteProduct =(id)=>(e)=>{
+    const done = window.confirm("do you want to delete the product");
+    
+    if(done){
+      axios.delete(`/product/${id}`,{headers:{authorization:`Bearer ${localStorage.getItem("access-admin")}`}}).then((res)=>console.log(res.data)).catch(err=>console.log(err));
+     
+    }
+    //
+  }
+
   return (
     <TableContainer component={Paper} sx={{my:3}}>
     <Table>
         <TableHead>
+        <TableCell>
+          image
+         </TableCell>
             <TableCell>Name</TableCell>
-            <TableCell>Category</TableCell>
-            <TableCell>Gender</TableCell>
+            <TableCell>Description</TableCell>
             <TableCell>Price</TableCell>
             <TableCell>Stock</TableCell>
-            <TableCell>Update</TableCell>
-            <TableCell>Delete</TableCell>
+            <TableCell></TableCell>
+            <TableCell></TableCell>
             <TableCell>Details</TableCell>
         </TableHead>
         <TableBody>
-        <TableRow>
-            <TableCell>Sweat shirt </TableCell>
-            <TableCell>SHIRT</TableCell>
-            <TableCell>Men</TableCell>
-            <TableCell>145</TableCell>
-            <TableCell>23</TableCell>
+       {filtered.map(item=><TableRow key={item._id}>
+          <TableCell> <img src={`data:image/jpeg;base64,${Buffer.from(item.images[0].data.data).toString('base64')}`} width="50px" alt=""/></TableCell>
+            <TableCell>{item.name}</TableCell>
+            <TableCell>{item.description}</TableCell>
+            <TableCell>{item.price}</TableCell>
+            <TableCell>{item.stock}</TableCell>
             <TableCell><Button size="small"><ModeOutlinedIcon color="success"/></Button></TableCell>
-            <TableCell><Button size="small"><DeleteOutlinedIcon color="error"/></Button></TableCell>
+            <TableCell><Button onClick={deleteProduct(item._id)} size="small"><DeleteOutlinedIcon color="error"/></Button></TableCell>
             <TableCell><Button size="small">More Details</Button></TableCell>
-        </TableRow>
+        </TableRow>)}
         </TableBody>
     </Table>
     </TableContainer>
